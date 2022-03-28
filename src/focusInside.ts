32b839890d0b711a88304e8be6465cc7e21b40d7
Pavel Shermayster <pavel.sh@openweb.com>
2022-03-28 16:04:06 +0300

    added focusInsideShadowDom function

--- src/focusInside.ts
+++ src/focusInside.ts
@@ -7,6 +7,22 @@ const focusInFrame = (frame: HTMLIFrameElement) => frame === document.activeElem
 const focusInsideIframe = (topNode: Element) =>
   Boolean(toArray(topNode.querySelectorAll<HTMLIFrameElement>('iframe')).some((node) => focusInFrame(node)));
 
+const focusInsideShadowDom = (activeElement: HTMLElement, node: Element): boolean => {
+  let currentElement = activeElement;
+
+  while (currentElement && currentElement.parentNode) {
+    if (currentElement.parentNode === node) {
+      return true;
+    } else if (currentElement.parentNode instanceof ShadowRoot) {
+      currentElement = currentElement.parentNode.host as HTMLElement;
+    } else {
+      currentElement = currentElement.parentNode as HTMLElement;
+    }
+  }
+
+  return false;
+};
+
 export const focusInside = (topNode: HTMLElement | HTMLElement[]): boolean => {
   const activeElement = document && getActiveElement();
 
@@ -15,7 +31,8 @@ export const focusInside = (topNode: HTMLElement | HTMLElement[]): boolean => {
   }
 
   return getAllAffectedNodes(topNode).reduce(
-    (result, node) => result || node.contains(activeElement) || focusInsideIframe(node),
+    (result, node) =>
+      result || node.contains(activeElement) || focusInsideIframe(node) || focusInsideShadowDom(activeElement, node),
     false as boolean
   );
 };
